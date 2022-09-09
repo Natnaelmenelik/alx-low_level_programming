7ddcccc7f8c0d4f186abe55ce8e7c869123d2513
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 04:49:58 -0700

    New commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,11 @@ int main(void) {
 	    long long int longintType;
 	    char charType;
 
-		printf("Size of char: %zu bytes\n", sizeof(charType));
-	        printf("Size of int: %zu bytes\n", sizeof(intType));
-	        printf("Size of long int: %zu bytes\n", sizeof(longType));
-		printf("Size of long long int: %zu bytes\n", sizeof(longintType));
-		printf("Size of float: %zu byte\n", sizeof(floatType));
+		printf("Size of a char: %zu bytes\n", sizeof(charType));
+	        printf("Size of an int: %zu bytes\n", sizeof(intType));
+	        printf("Size of a long int: %zu bytes\n", sizeof(longType));
+		printf("Size of a long long int: %zu bytes\n", sizeof(longintType));
+		printf("Size of a float: %zu byte\n", sizeof(floatType));
+		return (0);
 
 }
