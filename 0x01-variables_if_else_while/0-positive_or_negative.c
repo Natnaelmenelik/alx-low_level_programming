02a21777de4f58d60e3dc4f23d63c13dd7f48e59
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 07:18:23 -0700

    new commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+nclude <stdlib.h>
+#include <time.h>
+/* printing the stored number into +ve, -ve, or zero */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n>0)
+	{
+		printf("the number is positive \n");
+	}
+	else if(n=0)
+	{
+		 printf("the number is zero \n");
+	}
+	else{
+		 printf("the number is negative \n");
+	}
+	return (0);
+}
