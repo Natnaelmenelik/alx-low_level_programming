0926f81b608e89bd25f2a687ec1a33869a81f780
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 03:57:39 -0700

    Random

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+
+/**
+ *  * main -print the string using printf function
+ *   *
+ *    * using the main funvtion
+ *     *this program prints "with proper grammar, but the outcome is a piece of art"
+ *      *Return: 0
+ *       */
+
+int main(void)
+{
+	printf()
+	 printf( 
+	 printf()
+		 
+}
