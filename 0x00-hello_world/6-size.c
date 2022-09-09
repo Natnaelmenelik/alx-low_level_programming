0aaa8023895ab692919d3d95b974a42561eaccdd
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 04:22:51 -0700

    a bit crashed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,10 @@ include <stdio.h>
 
 int main(void)
 {
-	printf()
-	 printf( 
-	 printf()
-		 
+	printf("Size of a char");
+	 printf("Size of an int");
+	 printf("Size of a long int");
+         intf("Size of a long int"); 
+	 printf("Size of a float");
+	return (0);	
 }
