0861e1c51ed391a6e989317678d0ba3cd5061e3b
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 12:44:24 -0700

    New commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - printing base 10 numbers starting from 0
+ *Description: using the main fuction
+ *the program prints "prints base 10 numbers"
+ *Return: 0
+ */
+int main(void)
+{
+int mi;
+for (mi = 0 ; mi < 10 ; mi++)
+{
+        putchar(mi);
+}
+putchar("\n");
+return (0);
+}
