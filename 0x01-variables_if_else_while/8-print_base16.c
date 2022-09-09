5dd4f9ad50665f4b1183d67d423f778ae48999b1
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 13:13:00 -0700

    New commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - printing base 10 numbers starting from 0
+ *Description: using the main fuction
+ *the program prints "prints base 10 numbers"
+ *Return: 0
+ */
+int main(void)
+{
+	int mi;
+
+	for (mi = 48 ; mi < 66 ; mi++)
+	{
+		        putchar(mi);
+	}
+	putchar('\n');
+	return (0);
+}
