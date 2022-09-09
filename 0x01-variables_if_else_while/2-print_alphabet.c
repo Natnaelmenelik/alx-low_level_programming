fd5acd9b84566fae5d892d5745a2afe7d8719ce3
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-09 11:45:05 -0700

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - printing the word to lower case alphabtet
+ *Description: using the main fuction
+ **the program prints "prints in lower case"
+ *Return: 0
+ */
+int main(void)
+{
+char mi;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
