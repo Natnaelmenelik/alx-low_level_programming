2e93c1f1dc0241ae7803b68970da06a374b00a18
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 12:59:23 -0700

    Commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @c : used as parameter
+ *
+ * Return : 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+	return ("+");
+}
+else if (n = 0)
+{
+	return (0);
+}
+else
+{
+	return ("-");
+}
+}
