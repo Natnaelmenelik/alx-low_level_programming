4baa6f78e43c249b6bc38e895d88962d9a0b5512
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 12:28:53 -0700

    Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * int _islower(int c) - checks whether the letter is lowercase or uppercase
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	char mina = m;
+	int a = 49;
+	int b = 48;
+
+	if (islower(mina))
+{
+	_putchar(a);
+}
+else 
+{
+	 _putchar(b);
+}
+	_putchar(*"\n");
+	return (0);
+
+}
