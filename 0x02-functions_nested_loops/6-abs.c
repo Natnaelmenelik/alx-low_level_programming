1da6eb1c2536d7e3f7b70ae889b80471529d004f
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 13:49:53 -0700

    Commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 /**
- * abs - computes the absolute value
+ * _abs - computes the absolute value
  * @c : used as parameter
  * Return: 0 or 1
  */
 
 int _abs(int c)
 {
-	int d = abs (c);
+	int d = abs(c);
+
 	return (d);
 }
