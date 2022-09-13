bbd12aba952bd67a2cf1ebc1c0354039c307e867
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 15:34:08 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,19 +16,22 @@ int a = 9;
 h2 = 0;
 while (h2 <= 2)
 {
-if (h2 == 2)
+	if (h2 == 2)
 {
 	/*Restrain to 23h, not 29*/
 	a = 3;
 }
 	h1 = 0;
-while (h1 <= a)
+
+		while (h1 <= a)
 {
 	m2 = 0;
-while (m2 <= 5)
+
+			while (m2 <= 5)
 {
 	m1 = 0;
-while (m1 <= 9)
+
+				while (m1 <= 9)
 {
 																_putchar('0' + h2);
 	_putchar('0' + h1);
