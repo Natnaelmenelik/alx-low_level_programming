c0f6163f9ba40ff7037f6b48046276c521589196
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 11:08:18 -0700

    Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * print alphabet
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	for (char c = a, c <= z, c++)
+	{
+		_putchar(c);
+	}
+}
+
+int main(void)
+{
+	print_alphabet(void);
+	_putchar("\n");
+	return (0);
+}
