6c23362fc7a7934aa5ed93205f1ef6b3f9b11346
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 11:21:33 -0700

    Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /*
- * print alphabet
+ * print alphabet - prints alphabet in lowercase
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
