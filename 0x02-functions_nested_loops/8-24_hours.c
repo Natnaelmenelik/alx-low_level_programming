3127470aecdcd666316ff25bba50387df071b0d4
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 14:42:00 -0700

    Commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * jack_bauer - counts every minutes of a day
+ * Return : 0
+ */
+
+void jack_bauer(void);
+
+int sec;
+int min;
+
+for (sec = 0; sec <= 59; sec++)
+{
+	return (min);
+	_putchar("58");
+	return (sec)
+	min++;
+}
