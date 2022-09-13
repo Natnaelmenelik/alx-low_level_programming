12c706261dcced15e9d37c18b8d2f582ea880589
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-13 16:26:50 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers to 1 - 98
+ *
+ * Return:0 or 1
+ */
+
+void print_to_98(int n)
+{
+	while (n > 0 && n < 98 )
+	{
+	_putchar(n + '0');
+	n++;
+	_putchar(",");
+	}
+	_putchar("b");
+}
