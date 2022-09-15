82afc681034d705efb7a11b4c8fce2e30404b39e
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-15 13:49:16 -0700

    commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * mul - multiplies to integer
- * @a : int parameters
- * @b : second int parameters
+ * mul - multiplies two integer
+ * @a: int parameters
+ * @b: second int parameters
  * Return:0
  */
 
+
 int mul(int a, int b)
 {
-return(a * b);
+return (a * b);
 }
