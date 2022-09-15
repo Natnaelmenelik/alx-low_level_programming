d99e87193b15b78e7dd32be7ce1b832f861c45c0
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-15 13:31:35 -0700

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,15 @@
 /**
  * _isdigit - checks a digit
  * @c : the character
- * Return:0 or 1
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
-{
-	return(1);
-}
+	{
+		return(1);
+	}
+
 	return(0);
 }
