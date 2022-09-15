8ba06158296fed1a23330c0ff1ea1317c8fa63b6
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-15 12:43:32 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks a digit
+ * @c : used as a parameter
+ *
+ * Return:0 or 1
+ */
+
+int _isdigit(int c)
+{
+	while (c < 10)
+{
+	if (isdigit(c))
+{
+	return(1);
+}
+	else
+{
+	return(0);
+}
+}
+
+}
