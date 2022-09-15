2fb3efb1ad51c7772c0bc4fbdd803eba85819def
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-15 14:15:44 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the most numbers
+ * print_most_numbers - prints the numbers excluding 2 and 4
  * Return: 0
  */
 
@@ -9,12 +9,13 @@ void print_most_numbers(void)
 {
 	int c = 0;
 
-	while (c < 10)
+	while (c <= 9)
 	{
 		if (c != 2 && c != 4)
 		{
 			_putchar('0' + n);
 		}
+
 		c++;
 	}
 	_putchar(*"\n");
