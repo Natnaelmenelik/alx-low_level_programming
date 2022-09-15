e6653de60e1feb835cd9828581a82c7382fa36e9
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-15 12:26:13 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c - serves as parameter
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+
+{
+if (c >= 'A' &&  C <= 'Z')
+{
+	return (1);
+}
+else 
+{
+	return (0);
+}
+}
