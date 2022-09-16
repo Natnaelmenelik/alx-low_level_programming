6f71dec7cfd9f722933e39dd0211fd6c72d81a63
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-16 01:33:23 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,15 @@ void more_numbers(void)
 		{
 			for (c = 0; c <= 14; c++)
 			{
-				if (n > 9)
+				if (c > 9)
 			{
 				_putchar((c / 10) + '0');
 
 			}
 
-			_putchar((c / 10) + '0');
+				_putchar((c / 10) + '0');
 			}
+			_putchar(10);
 		}
+}
 
