a22949dc99d023a5dd78ecd209dfac0b3f2e64bb
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-16 14:27:38 -0700

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks a number
+ * @i - parameter
+ * Return: 0 or 1
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTAN_H_INCLUDED
+
+void positive_or_negative(int i);
+
+#endif
