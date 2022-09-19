f0ac5c4b682b95fb7c092c242af647d066fb89a7
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 13:03:00 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str : parameter
+ *
+ * Return :0 or 1
+ */
+
+void _puts(char *str)
+{
+	return (puts(str));
+}
