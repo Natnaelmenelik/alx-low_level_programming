ab980fc8a9951757d27466a015f8cb0793601597
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 13:50:55 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,26 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * print_rev - prints a string in a reverse matter
- * @s : parameter
- * Return: 0 or 1
+ * rev_string - prints a string in reverse
+ * @s: the used string to be reversed
+ * Return: 0
  */
 
 void rev_string(char *s)
 {
-	int g, numb;
-	int tmpry = 0;
+	int len, i, half;
+	char temp;
 
-	for(numb=0; s[numb] != 0; numb++);
-	for(g = 0; g <numb/2; g++)
+	for (len = 0; s[len] != '\0'; len++)
+
+	i = 0;
+	half = len / 2;
+
+	while (half--)
 	{
-		tmpry = s[g];
-		s[g]=s[numb - 1 - g];
-		s[numb - 1 - g] = tmpry;
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
 	}
-	for(g = 0; g < numb; g++)
-		_putchar(s[g]);
 }
