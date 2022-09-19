bb6a68b6239858f9278947498736c39c813d0345
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 14:35:50 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str : parameter
+ * Return: 0 or 1
+ */
+
+void puts_half(char *str)
+{
+	unsigned int n, i, half;
+
+	n = (strlen(str) - 1) / 2;
+
+	half = (strlen(str) / 2) + 1;
+
+	if (strlen(str) % 2 == 0)
+	{
+		while (strlen(str) != " ")
+		{
+			_putchar(str[half]);
+		}
+	}
+	else
+	{
+		_putchar(str[n]);
+	}
+	_putchar(*"\n");
+}
