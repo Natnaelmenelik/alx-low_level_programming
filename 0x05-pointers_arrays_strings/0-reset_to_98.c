4a110514e597a04a361ba248e58f693e28b0b15d
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 05:20:12 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates its value to 98
+ * @*n : parameter
+ * Return: 0 or 1
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = &n;
+
+	*p = 48;
+}
