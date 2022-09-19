4a87a075cb42b2f2a04b4b88f2d3d20780c195b9
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 13:57:49 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character
+ * @str : parameter
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+}
