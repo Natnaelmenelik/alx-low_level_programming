be34c3e694d8f9e64f2f9195994e0dc26ac57148
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 13:30:26 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in a reverse matter
+ * @s : parameter
+ * Return: 0 or 1
+ */
+
+void print_rev(char *s)
+{
+        int i = 0;
+
+        while (s[i])
+                i++;
+
+        while (i--)
+                _putchar(s[i]);
+}
