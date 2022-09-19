a2256dd3fd1aab5e3f0de13009cc2bc226cb0b03
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 16:37:09 -0700

    commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	while (i < n && i != (n - 1))
+	{
+		printf("%d", a[i]);
+		printf(", ");	
+	}
+	/*for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		printf(", ");
-	}
+	}*/
 
 	printf("\n");
 }
