d12ec009a3a0e732be2032e60e087454d122eecd
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-19 16:55:32 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcpy - copy a string from src to dest
+ * @dest : destination
+ * @src : source
+ * Return: 0 or 1
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	_putchar(dest);
+}
