5efca484cc7f0de183ba1b67ef8309dc91161a7d
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-22 01:09:31 -0700

    commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = n - 1; i >= n / 2; i--)
 	{
 		tmp = a[n -1 - i];
-		a[n - 1 -i] = a[i];
+		a[n - 1 - i] = a[i];
 		a[i] = tmp;
 	}
 }
