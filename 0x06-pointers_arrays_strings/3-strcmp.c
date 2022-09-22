1d5c2f7a9b41c5857dc26c0f53cc50b6587308b4
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-21 23:56:33 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1 : first string
+ * @s2 : second string
+ * Return: 0 or 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+}
