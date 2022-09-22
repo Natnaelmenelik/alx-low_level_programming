200b801cb1aee652adc045d7ee5428cf7bb7a4f3
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-22 01:14:35 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lower case to upper
+ * @str : string input
+ * Return: 0 or 1
+ */
+
+char *string_toupper(char *str)
+{
+	strupr(str);
+	return (str);
+}
