d5272443d9b4977a319b0c41e9a253146e762763
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-21 23:33:20 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcat - concatnates two strings
+ * @dest : destination
+ * @src : source
+ * Return: 0 or 1
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest,src);
+	return (dest);
+}
