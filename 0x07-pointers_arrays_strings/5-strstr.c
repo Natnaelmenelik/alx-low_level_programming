74d5fdd27c52275013a9f76521fd2954cd838919
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-26 13:02:23 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack - initial string
+ * @needle - string to be compared
+ * Return: a pointer to the beginning of the located substrin or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *mina;
+
+	mina = strstr(haystack, needle);
+
+	return (mina);
+}
