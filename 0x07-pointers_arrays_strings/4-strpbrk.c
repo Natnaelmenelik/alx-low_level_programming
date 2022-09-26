a7d3e8b1939369e21deb488b8d57c1c53b68403a
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-26 12:32:03 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes.
+ * @s : string 1
+ * @accept : string 2
+ * Return: a pointer to the byte in s that matches one of the bytes in accept, or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *fnd;
+
+	fnd = strpbk(s, accept);
+	if (fnd != NULL)
+	{
+		return (fnd);
+	}
+}
