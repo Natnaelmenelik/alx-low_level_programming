2afe3024267944abbf2f88509eada8d5198c0614
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-26 04:36:47 -0700

    Header file included

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_memset - function that fills memory with a constant byte.
+ * @s : string
+ * @b : the value to be set
+ * @b : number of bytes
+ * Return: 0 or 1;
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(str, 'b', n);
+	return (*s);
+}
