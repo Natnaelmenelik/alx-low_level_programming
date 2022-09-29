e599e4958ae6ddc3857b3f5bd880531d82e263b9
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-29 01:35:48 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints length of a string
+ * @s : string
+ * Return : lenth of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	strlen(s);
+}
