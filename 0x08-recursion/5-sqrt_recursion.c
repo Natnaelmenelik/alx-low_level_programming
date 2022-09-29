7f23eedb7a51ff155d47d373f80e0c11310fa11c
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-29 02:35:31 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - returns a square root of a number
+ * @n : the number
+ * Return: sqaure root of a number or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x = n;
+
+	s = sqrt(n);
+	return (s);
+}
