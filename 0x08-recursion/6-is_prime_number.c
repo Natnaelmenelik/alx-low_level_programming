e6b7654fef9b5c40d1e85f3d5635db72b854ecd0
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-29 03:04:56 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if it is prime number
+ * @n : the number
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n % n == 0 && n % 1 == 0)
+	{
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
