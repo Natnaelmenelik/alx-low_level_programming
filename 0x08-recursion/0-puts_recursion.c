5a2663c5f9ff8e82ba00d379a996666de2cff74d
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-29 00:51:46 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s : the string to be printed
+ * Return: String
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+	_putchar("\n");
+}
