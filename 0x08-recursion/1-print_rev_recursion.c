bbef053fed098c707ec052395617818d51d8ab37
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-09-29 01:15:49 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _print_rev_recursion - prints reverse in string
+ * @s : the string
+ * Return: reversed string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	strrev(s);
+}
