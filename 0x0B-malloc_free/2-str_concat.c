67ba774333451b8a3c619df52a5443af52b22c4f
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-10-03 14:50:15 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	/*
+
 	for (i = 0; i < len; i++)
 	{
 		if (s1[i] == "")
@@ -39,8 +39,8 @@ char *str_concat(char *s1, char *s2)
 		{
 			return("");
 		}
-	}*/
-	mina = strcat(*s1, *s2);
+	}
+	mina = strcat(s1, s2);
 	putchar(*mina);
 	return (0);
 }
