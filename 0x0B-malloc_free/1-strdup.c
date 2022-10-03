9753da5248c475239e57d4849f6cf92c8aeb89c0
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-10-03 14:00:58 -0700

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,13 +19,13 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 
-	s = malloc(sizeof(*char));
-	
+	s = malloc(sizeof(char));
+	/*
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
+	*/
 	mina = strdup(str);
 	free(s);
 	return (mina);
