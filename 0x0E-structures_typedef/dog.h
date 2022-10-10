fc75a7acbb8335e0f090c97ad677e78cffdc352a
Natnaelmenelik <natnaelmenelik3@gmail.com>
2022-10-10 02:58:38 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
  * struct dog - new user defined variable
  * @name : name of the owner
